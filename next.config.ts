7380539267073566e89bf798152e5ebe523852b4
amjido-01 <youndsadeeq10@gmail.com>
2025-06-19 15:04:23 +0100

    next config

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://webtray-api.onrender.com/api/v1/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
