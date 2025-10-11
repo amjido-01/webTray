8b8e3f0f11e6dbc27fa953bbe8b80757cf47f838
Abubakar Muhammad Ala <youndsadeeq10@gmail.com>
2025-10-11 19:21:22 +0100

    landing page fixes

--- components/all-in-one.tsx
+++ components/all-in-one.tsx
@@ -7,10 +7,10 @@ export function AllInOne() {
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 md:gap-10 lg:gap-20 items-center">
           {/* Left Content */}
           <div className="space-y-6">
-            <h1 className="text-center mb-[16px] text-[#1A1A1A] md:text-start text-[24px] font-bold md:text-[40px] lg:text-6xl text-balance">
+            <h2 className="text-center mb-[16px] text-[#1A1A1A] md:text-start text-[24px] font-bold md:text-[40px] lg:text-6xl text-balance leading-[100%] md:text-5xl md:mb-[24px] x">
               All You Need in One Platform.
-            </h1>
-            <p className="text-lg md:text-xl text-center md:text-start text-[#4D4D4D] font-normal text-[16px] leading-relaxed">
+            </h2>
+            <p className="text-center md:text-start text-[#4D4D4D] font-normal text-[16px] leading-[24px] text-pretty">
              Switch to a single platform built to simplify operations, reduce stress, and help your business scale.
             </p>
           </div>
--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { Button } from "@/components/ui/button"
 import Header from "@/components/header";
-import hero_img from "@/public/hero_img.png";
+import hero from "@/public/hero.png";
 import Image from "next/image";
 import confeti_right from "@/public/confeti_right.svg"
 import confeti_left from "@/public/confeti_left.svg"
@@ -13,16 +13,16 @@ export default function Hero() {
     <div className="min-h-screen bg-gray-50">
       <Header />
 
-      <section className="max-w-7xl mx-auto md:mt-[70px] px-4 sm:px-6 lg:px-8 pt-16 pb-8">
+      <section className="max-w-7xl mx-auto mt[40px] md:mt-[80px] px-4 sm:px-6 lg:px-8 pt-16 pb-8">
         <div className="text-center relative">
-          <h1 className="text-5xl font-extrabold md:text-[80px] leading-[100%] text-gray-900">
+          <h1 className="text-4xl md:text-5xl font-extrabold md:text-[80px] leading-[100%] text-gray-900">
             Your Business, <span className="bg-gradient-to-br from-[#365BEB] to-[#9233EA] bg-clip-text text-transparent">Simplified</span>
           </h1>
-          <p className="text-[16px] md:mt-[24px] font-normal leading-6 text-[#4D4D4D] max-w-3xl mx-auto">
+          <p className="text-[16px] mt-[24px] font-normal leading-[24px] text-[#4D4D4D] max-w-3xl mx-auto">
             WebTray is the all-in-one SaaS platform for restaurants and retail shops. Manage inventory, create your
             online store, and grow your business with ease.
           </p>
-          <div className="flex md:mt-[40px] flex-col sm:flex-row gap-4 justify-center">
+          <div className="flex mt-[40px] items-center sm:flex-row gap-4 justify-center">
             <Button size="lg"  onClick={() => router.push("/signup")} className="bg-[#111827] hover:bg-[#30343e] cursor-pointer text-[16px] font-normal rounded-full text-white px-[16px] py-[14px]">
               Sign Up for Free
             </Button>
@@ -36,8 +36,8 @@ export default function Hero() {
 
         </div>
 
-       <div className="mt-[88px]">
-        <Image src={hero_img} alt="hero image" />
+       <div className="mt-[64px] md:mt-[88px]">
+        <Image src={hero} alt="hero image" className="md:w-[80%] md:h-auto object-cover mx-auto"/>
        </div>
       </section>
     </div>
